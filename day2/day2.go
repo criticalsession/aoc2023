71ff45f5b96c55010ea011693a595bf48089e663
Critical Session <criticalsession@protonmail.ch>
2023-12-30 23:59:18 +0100

    day 2

---
+++ day2/day2.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/criticalsession/aoc2023/utils"
+
+func main() {
+	s, err := utils.GetInput("day2sample.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	solve(s, false)
+}
+
+func solve(s []string, part1 bool) {
+
+}
