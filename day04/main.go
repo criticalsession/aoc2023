146e41578d850b31fec48133cad1450c1770b5b0
Critical Session <criticalsession@protonmail.ch>
2023-12-31 08:08:33 +0100

    all my homies love scratchcards

--- day04/main.go
+++ day04/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/criticalsession/aoc2023/utils"
+import (
+	"fmt"
+	"math"
+
+	"github.com/criticalsession/aoc2023/utils"
+)
 
 func main() {
 	s, err := utils.GetInput(utils.InputOptions{
-		Path: "sample.txt",
+		Path:  "input.txt",
+		Split: ":",
 	})
 	utils.Catch(err)
 
@@ -17,4 +23,30 @@ func solve(s []string, partTwo bool) {
 	// 2. for each number owned, check set
 	// 3. total points = 2^(count-1)
 
+	total := 0
+	for _, l := range s {
+		card := utils.SplitAndTrim(l, "|")
+		win := utils.SplitAndTrim(card[0], " ")
+		nums := utils.SplitAndTrim(card[1], " ")
+
+		test := make(map[int]bool, 5)
+		for _, n := range win {
+			if n != "" {
+				test[utils.ConvertToNumber(n)] = true
+			}
+		}
+
+		var c int8 = 0
+		for _, n := range nums {
+			if n != "" && test[utils.ConvertToNumber(n)] {
+				c++ // hehe
+			}
+		}
+
+		if c > 0 {
+			total += int(math.Pow(2, float64(c-1)))
+		}
+	}
+
+	fmt.Println(total)
 }
