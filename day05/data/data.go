e40285200c1acc6fb181beac00fda755c1cd0265
Critical Session <criticalsession@protonmail.ch>
2024-01-02 19:35:32 +0100

    started walk with ranges for part 2

--- day05/data/data.go
+++ day05/data/data.go
@@ -1,19 +1,26 @@
 package data
 
+import "fmt"
+
 type ParsedData struct {
 	Seeds       []int
 	RangeBlocks []RangeBlock
 }
 
-func (p *ParsedData) GetSeedRanges() [][]int {
-	ranges := [][]int{}
+type StartEnd struct {
+	Start int
+	End   int
+}
+
+func (p *ParsedData) GetSeedRanges() []StartEnd {
+	ranges := []StartEnd{}
 	seedStart := -1
 	for _, s := range p.Seeds {
 		if seedStart < 0 {
 			seedStart = s
 			continue
 		} else {
-			ranges = append(ranges, []int{seedStart, seedStart + s})
+			ranges = append(ranges, StartEnd{seedStart, seedStart + s})
 			seedStart = -1
 		}
 	}
@@ -47,4 +54,29 @@ func (r *RangeBlock) GetDestValue(s int) int {
 
 	//sny source numbers that aren't mapped correspond to the same destination number.
 	return s
+}
+
+func (r *RangeBlock) ConvertToOverlappingRanges(s int, e int) {
+	fmt.Println(s, e)
+
+	results := []StartEnd{}
+	for i := range *r {
+		sourceStart := (*r)[i].SourceStart
+		sourceEnd := (*r)[i].SourceStart + (*r)[i].Width
+		fmt.Println("checking:", sourceStart, sourceEnd)
+
+		if e >= sourceStart && s < sourceEnd {
+			fmt.Println("overlap")
+
+			// overlaps
+			overlapStart := max(s, sourceStart)
+			overlapEnd := min(e, sourceEnd)
+			results = append(results, StartEnd{
+				overlapStart - sourceStart + (*r)[i].DestStart,
+				overlapEnd - sourceStart + (*r)[i].DestStart,
+			})
+		}
+	}
+
+	fmt.Println(results)
 }
